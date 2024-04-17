9df611ecd66185e38ccc18570f10a8ff0f730c9a
saro1821 <148046220+saro1821@users.noreply.github.com>
2024-04-17 21:31:07 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import {initializeApp} from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 const firebaseConfig = {
     apiKey: "AIzaSyBWqaztrL4zNtIZJpE3o0FeGTzjIgWCwBI",
-    authDomain: "server-1-ceom.onrender.com",
+    authDomain: "mern-chatapps.firebaseapp.com",
     projectId: "mern-chatapps",
     storageBucket: "mern-chatapps.appspot.com",
     messagingSenderId: "616871615821",
